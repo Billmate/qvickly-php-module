4174202f2b2698757474581deca638226fa7aae7
Thomas Björk <107463939+codedeviate@users.noreply.github.com>
2024-07-08 13:40:14 +0200

    Updated README and fixed static call

--- src/Paylink/PaylinkAPI.php
+++ src/Paylink/PaylinkAPI.php
@@ -11,23 +11,33 @@ class PaylinkAPI
 {
     protected PaymentAPI $paymentAPI;
 
-    public function __construct(private readonly string $eid, private readonly string $secret, private readonly bool $testMode = false)
+    public function __construct(
+        private readonly string $eid,
+        private readonly string $secret,
+        private readonly bool $testMode = false,
+        private array $overrides = []
+    )
     {
-        $this->paymentAPI = new PaymentAPI($this->eid, $this->secret, $this->testMode);
+        $this->paymentAPI = new PaymentAPI(eid: $this->eid, secret: $this->secret, testMode: $this->testMode, overrides: $this->overrides);
     }
 
     public function __call(string $name, array $arguments)
     {
-        match($name) {
-            'create' => $this->create(...$arguments),
+        return match($name) {
+            'create' => call_user_func_array(array($this, 'create'), $arguments),
             default => throw new \Exception('Method not found')
         };
     }
 
     public static function __callStatic(string $name, array $arguments)
     {
-        match($name) {
-            'create' => (new self(...$arguments))->create(...$arguments),
+        $eid = defined('QVICKLY_PAYLINK_API_EID') ? constant('QVICKLY_PAYLINK_API_EID') : $_ENV['EID'];
+        $secret = defined('QVICKLY_PAYLINK_API_SECRET') ? constant('QVICKLY_PAYLINK_API_SECRET') : $_ENV['SECRET'];
+        if(!isset($eid) || !isset($secret)) {
+            throw new \Exception('EID and SECRET must be set either as constants or in the environment variables');
+        }
+        return match($name) {
+            'create' => call_user_func_array(array(new self($eid, $secret), 'create'), $arguments),
             default => throw new \Exception('Method not found')
         };
     }
--- src/Paylink/README.md
+++ src/Paylink/README.md
@@ -1,3 +1,43 @@
 # Qvickly Paylink API
 
 > The Paylink API is more of an implemented services that uses the Payment API to create a payment link.
+
+## Installation
+
+```bash
+composer require qvickly/api
+```
+
+## Usage
+
+```php
+<?php
+use Qvickly\Api\Paylink\PaylinkAPI;
+
+$paylinkAPI = new PaylinkAPI($eid, $secret);
+$data = [];
+// Build data array
+$payment = $paylinkAPI->create($data);
+
+// Redirect user to $payment['url'] if it is set
+```
+This code does the same thing as the following code:
+```php
+<?php
+use Qvickly\Api\Payment\PaymentAPI;
+
+$paymentAPI = new PaymentAPI($eid, $secret);
+$data = new Data([]);
+// Build data array
+$data->updateCart();
+$payment = $paymentAPI->addPayment($data);
+
+// Redirect user to $payment['url'] if it is set
+```
+
+## Functions
+
+### create($data, $roundCart = null)
+This function takes the data, which can be either an array or a Data object, and creates a payment link. Before the addPayment function is called, the data is updated with the cart, so there is no need to calculate the cart total.
+
+The second parameter is optional and is used to round the cart total. If the cart total is 100.50 and the roundCart parameter is set to 1, the cart total will be 101.00. This is used by adding a rounding attribute to the total.
