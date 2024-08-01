552c3b721afdba1a09dd5cc0cda147e46ea9ce70
Thomas Björk <107463939+codedeviate@users.noreply.github.com>
2024-08-01 12:57:04 +0200

    Updated checkoutStatus example

---
+++ examples/CheckoutAPI/A-checkoutStatus.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../..');
+$dotenv->load();
+
+use Qvickly\Api\Checkout\CheckoutAPI;
+
+use function Qvickly\Api\Payment\Helpers\exampleCheckout;
+use Qvickly\Api\Enums\PaymentMethod;
+
+$checkoutAPI = new CheckoutAPI($_ENV['EID'], $_ENV['SECRET']);
+
+echo "Create checkout\n";
+$payload = exampleCheckout();
+$checkout = $checkoutAPI->initCheckout($payload);
+
+echo "Step 1\n";
+$personalInfo = $checkoutAPI->step1($checkout['hash'], [
+    'pno' => $_ENV['PNO'],
+    'email' => $_ENV['EMAIL'],
+    'type' => 'person',
+    'zip' => $_ENV['ZIP'],
+    'phonenumber' => $_ENV['PHONENUMBER'],
+]);
+
+echo "Update payment method\n";
+$paymentMethod = $checkoutAPI->updatePaymentMethod($checkout['hash'], PaymentMethod::CARD->value);
+
+echo "Get checkout status\n";
+$status = $checkoutAPI->checkoutStatus($checkout['hash']);
+
+echo json_encode($status, JSON_PRETTY_PRINT) . "\n";
+
+echo "URL to use: " . $checkout['url'] . "\n";
