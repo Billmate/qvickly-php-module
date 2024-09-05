8da859092a5ad83afab557836f1d36a9f5b6011c
Thomas Björk <107463939+codedeviate@users.noreply.github.com>
2024-09-05 10:51:21 +0200

    Create getStatuslogs.php
    Added example for getStatuslogs

---
+++ examples/PortalAPI/getStatuslogs.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+use Qvickly\Api\Portal\PortalAPI;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../..');
+$dotenv->load();
+
+$portalAPI = new PortalAPI($_ENV['TOKEN'], testMode: true);
+$invoices = $portalAPI->get('invoices', params: ['filter' => 'search', 'search' => 'Testperson']);
+if(is_array($invoices) && array_key_exists('error', $invoices)) {
+    echo "Code: " . $invoices['code'] . "\n";
+    echo "Error: " . $invoices['error'] . "\n";
+    exit;
+}
+echo "Found " . count($invoices) . " invoices\n";
+if(count($invoices) > 0) {
+    $oneInvoice = array_pop($invoices);
+    $oneInvoiceId = $oneInvoice['invoiceid_real'];
+    $statusLog = $portalAPI->get('invoices/' . $oneInvoiceId . '/statuslog');
+    echo "And the statuslog for the last invoice is:\n";
+    echo json_encode($statusLog, JSON_PRETTY_PRINT);
+}
+
+
